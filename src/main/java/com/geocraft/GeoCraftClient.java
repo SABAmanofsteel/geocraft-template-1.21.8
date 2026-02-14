8872860abbce8e4db1e0b43fcf875dccfe21c1db
Saba <sabajoglidze@gmail.com>
2026-02-14 21:18:57 +0100

    Initial commit

---
+++ src/main/java/com/geocraft/GeoCraftClient.java
@@ -0,0 +1,11 @@
+package com.geocraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GeoCraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/geocraft/GeocraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.geocraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GeocraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
