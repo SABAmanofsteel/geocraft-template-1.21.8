260d0773977f4b342b2ad21392b15cd31b929498
Saba <sabajoglidze@gmail.com>
2026-02-14 21:47:04 +0100

    khinkali name

--- src/main/java/com/geocraft/item/ModItems.java
+++ src/main/java/com/geocraft/item/ModItems.java
@@ -2,6 +2,7 @@ package com.geocraft.item;
 
 import com.geocraft.Geocraft;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
