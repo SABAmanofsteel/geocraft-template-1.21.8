e338c4fee556f8162e45b6df6b092c1cbb54cb87
Saba <sabajoglidze@gmail.com>
2026-02-14 22:12:36 +0100

    khinkali name

--- src/main/java/com/geocraft/item/ModItems.java
+++ src/main/java/com/geocraft/item/ModItems.java
@@ -6,11 +6,13 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 
 public class ModItems {
-    public  static final Item KHINKALI = registerItem("khinkali", new Item(new Item.Settings()));
+    public static final Item KHINKALI=registerItem("khinkali",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Geocraft.MOD_ID,"khinkali")))));
 
     private  static  Item registerItem(String name,Item item){
         return Registry.register(Registries.ITEM, Identifier.of(Geocraft.MOD_ID,name),item);
